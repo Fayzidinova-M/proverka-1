b9f045c7e3b8c4df26130496a2a4583cdf7933c9
Fayzidinova M <fayzidinovam98@gmail.com>
2023-01-30 16:02:47 +1000

    слайс, сплайс, реверс, конкат

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
-//Massiv
-const users=['anna','vika','katya'];
-console.log(users);
-users[2]='kristina';
-console.log(users);
+const roles=['superUser','iAm','lol','user','admin','manager'];
 
-const arrLength=users.push('nikita');
-/* хамин push бади охирон элемент якти дига 
-элемент куш мекунад агар unshift нависим
-а хама пеш якти дига эл куш мекунад
-*/
-console.log(users);
-console.log(arrLength);
-users.unshift('vasya');
-console.log(users);
-users.pop();
-users.shift();
-console.log (users);
-users.pop();
-users.shift();
-console.log(users);
-/*
-хаминчашанда оператори 
-поп а акибу оператори 
-шифт а пеш якта якта элемента 
-удалит мекунад
-*/
-
+let elIndex=roles.indexOf('admin');
+console.log(elIndex);
+// ин индекси элемента меевад
+const res=roles.slice(2,4);
+// ин оператор мебурад а 2юм эл (2куш) то 4ум элемента (4 кушне)лекин массивба намерасад
+console.log(res);
+console.log(roles);
+const res2=roles.splice(2,2);
+//инаш якбора а массив мебурад хам сони а эл 2 сар кара 4та ояндаша
+console.log(res2);
+console.log(roles);
+const res4=roles.reverse();
+console.log(res4);
+console.log(roles);
+//инам сплайсбоин мссава дига мекунад ин поскуно е чаппа мекунад
+// бо оператори конкат хай вай дута массива конкатенатсия мекунад 2 та массива
