61cde77249a4ad7f8fe88612dd92fd9d830b3013
Fayzidinova M <fayzidinovam98@gmail.com>
2023-01-31 20:56:21 +1000

    continue; break в цикле

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-for (let i=1;i<10;i++){
-console.log(`наш баланс ${i}$`);
+let i=Number;
+let tasks=new Array();//тарзи эълонкунии МАССИВ
+for (i=0;i<=10;i++){
+    tasks[i]='задача '+i;
+    console.log(tasks[i]);
 }
+console.log('.........');
+for (i=0;i<=10;i++){
+    if (tasks[i]==='задача 2'){
+        continue;
+    }
+    console.log(tasks[i]);
+}
+console.log('.........');
+for (i=0;i<=10;i++){
+    if (tasks[i]==='задача 2'){
+        break;
+    }
+    console.log(tasks[i]);
+}
+//Фарки байни континю кати брик-а беннн
+//континю хамуншартанги катора пафта давомаша кор кар
+//брик шартба рафта а сикл бурумад888
+//888888888888888888888
